6865332f07ac10e618d68283d1aee6994a835189
Martin Duran <martinduran@Martins-MacBook-Pro.local>
2017-01-02 21:50:44 -0600

    Documents createParser

--- src/createParser.js
+++ src/createParser.js
@@ -1,9 +1,15 @@
+/**
+ * Returns a parser function meant to parse
+ * element attributes (.e.g., [data-each="parent.state.items"])
+ *
+ * @param  {String} body
+ * @param  {Object[]} args
+ * @return {Function}
+ */
 export default function createParser(body, ...args) {
   // eslint-disable-next-line no-new-func
-  return new Function(...args, `
-    try {
+  return new Function(...args,
+    `try {
       ${body};
-    } catch (e) {
-
-    }`);
+    } catch (e) {}`);
 };

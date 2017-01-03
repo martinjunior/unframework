7a6886ef1daafa08ab23dd661f8ab6091289736c
Martin Duran <martinduran@Martins-MacBook-Pro.local>
2017-01-02 21:48:54 -0600

    Documents utils

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,11 @@
+/**
+ * Returns the first valid reference node.
+ * A reference node is valid when it is contained by the document.body.
+ *
+ * @function findReferenceNode
+ * @param {HTMLElement[]} nodes
+ * @returns {?HTMLElement}
+ */
 export function findReferenceNode(nodes = []) {
   let ref = null;
 
@@ -14,16 +22,41 @@ export function findReferenceNode(nodes = []) {
   return ref;
 }
 
+/**
+ * Determines if the provided node is a valid node reference.
+ *
+ * @function isValidReferenceNode
+ * @param {HTMLElement} node
+ * @returns {Boolean}
+ */
 function isValidReferenceNode(node) {
   return node && document.body.contains(node);
 }
 
+/**
+ * Returns an array of next siblings.
+ * Next siblings are described as the nodes
+ * that follow the provided el.
+ *
+ * @function findNextSiblings
+ * @param {HTMLElement} el
+ * @param {HTMLElement[]} children
+ * @returns {HTMLElement[]} - next siblings
+ */
 export function findNextSiblings(el, children) {
   const childenArray = [...children];
-  return childenArray.slice(childenArray.indexOf(el), childenArray.length);
+  return childenArray.slice(childenArray.indexOf(el) + 1, childenArray.length);
 }
 
-
+/**
+ * Returns an object containing reference nodes
+ * found within the provided el.
+ *
+ * @function findRefs
+ * @param {HTMLElement} el
+ * @param {Component[]} children
+ * @returns {Object} - element references
+ */
 export function findRefs(el, children) {
   const refs = {};
 
@@ -41,6 +74,16 @@ export function findRefs(el, children) {
   return refs;
 }
 
+/**
+ * Returns the children found immediately
+ * within the provided el. Immediate children are
+ * children that are not nested within each other.
+ *
+ * @function findImmediateChildren
+ * @param {HTMLElement} el
+ * @param {String} selector
+ * @returns {HTMLElement[]} - immediate children
+ */
 export function findImmediateChildren(el, selector) {
   const immediateChildren = [];
   const children = [...el.querySelectorAll(selector)];

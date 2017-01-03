4f44b765613eaed6870933d0b64a24b3fe507414
Martin Duran <martinduran@Martins-MacBook-Pro.local>
2017-01-02 21:51:04 -0600

    Documents EachItemComponent

--- src/EachItemComponent.js
+++ src/EachItemComponent.js
@@ -1,3 +1,13 @@
 import Component from './Component';
 
+/**
+ * A component used to parent child
+ * components of EachComponents.
+ *
+ * @class EachItemComponent
+ * @constructor
+ * @param {HTMLElement} el
+ * @param {Component} parent
+ * @extends Component
+ */
 export default class EachItemComponent extends Component {}
